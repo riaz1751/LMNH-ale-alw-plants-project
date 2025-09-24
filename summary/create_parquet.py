e517116e76f041752328fe6d1e54bf0f7bbe78e3
riaz <riaz.rahman1751@gmail.com>
2025-09-24 18:36:59 +0100

    Upload summary data to S3 bucket

--- summary/create_parquet.py
+++ summary/create_parquet.py
@@ -1,30 +1,69 @@
+"""This script stores summary data in parquet files in an AWS S3 Bucket."""
+
+
 import pandas as pd
 from dotenv import load_dotenv
+from os import getenv
+import awswrangler as wr
+import boto3
+import pyodbc
 from connect_db_utils import get_connection, query_db
 from extract import get_average_soil_moisture_df, get_average_temp_df
 
-def export_to_parquet():
-    load_dotenv()
-    conn = get_connection()
-
+def export_to_parquet(my_session: boto3.session.Session, conn: pyodbc.Connection, bucket: str):
+    """upload summary data to parquet files in S3 bucket."""
     reading_data = pd.DataFrame.from_records(
         query_db(conn, "SELECT * FROM beta.Reading;"),
         columns=['reading_id', 'temperature', 'soil_moisture', 'recording_taken', 'plant_id', 'botanist_id']
     )
 
+    reading_data["recording_taken"] = pd.to_datetime(
+        reading_data["recording_taken"], errors="coerce"
+    )
+
     avg_temp = get_average_temp_df(reading_data)
     avg_moisture = get_average_soil_moisture_df(reading_data)
 
-    avg_temp.to_parquet("average_temperature",
-                        partition_cols=["recording_taken"],
-                        index=False)
-    avg_moisture.to_parquet("average_soil_moisture", 
-                        partition_cols=["recording_taken"],
-                        index=False)
+    avg_temp["hour"] = avg_temp["recording_taken"]
+    avg_moisture["hour"] = avg_moisture["recording_taken"]
 
-    print("It worked")
 
-    conn.close()
+    s3_path_temp = f"s3://{bucket}/etl-output/average_temperature/"
+    s3_path_moist = f"s3://{bucket}/etl-output/average_soil_moisture/"
+
+    wr.s3.to_parquet(
+        df=avg_temp,
+        path=s3_path_temp,
+        dataset=True,
+        partition_cols=["hour"],
+        boto3_session=my_session,
+        mode="overwrite"
+    )
+
+    wr.s3.to_parquet(
+        df=avg_moisture,
+        path=s3_path_moist,
+        dataset=True,
+        partition_cols=["hour"],
+        boto3_session=my_session,
+        mode="overwrite"
+    )
+
+    print(f"Data has been written to {s3_path_temp} and {s3_path_moist}")
 
 if __name__ == "__main__":
-    export_to_parquet()
+
+    load_dotenv()
+
+    conn = get_connection()
+    
+    my_session = boto3.session.Session(
+        aws_access_key_id=getenv('AWS_ACCESS_KEY'), 
+        aws_secret_access_key=getenv('AWS_SECRET_ACCESS_KEY'),
+        region_name=getenv('AWS_REGION')
+        )
+    bucket = getenv("S3_BUCKET")
+        
+    export_to_parquet(my_session, conn, bucket)
+
+    conn.close()
