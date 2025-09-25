1254bc010e3c3646a20f45f93d20ca8e1a78c299
Nicanor Enguerra <nenguerra@gmail.com>
2025-09-25 15:59:35 +0100

    Uncomment summary lambda terraform

--- terraform/stage_two/main.tf
+++ terraform/stage_two/main.tf
@@ -90,6 +90,51 @@ resource "aws_ecs_task_definition" "c19-cran-plants-pipeline" {
   )
 }
 
+# Lambda Function
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "c19-cran-lambda-role" {
+  name               = "c19-cran-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_function" "c19-cran-plants-summarise-lambda" {
+  function_name = "c19-cran-plants-summarise"
+  role          = "arn:aws:iam::129033205317:role/c19-cran-lambda-role"
+  package_type  = "Image"
+  image_uri     = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c19-cran-summarise:latest"
+
+  environment {
+    variables = {
+      ACCESS_KEY = var.ACCESS_KEY,
+      SECRET_ACCESS_KEY = var.SECRET_ACCESS_KEY,
+      DB_DRIVER = var.DB_DRIVER,
+      S3_BUCKET = var.S3_NAME,
+      REGION = var.REGION,
+      DB_HOST = var.DB_HOST,
+      DB_PORT = var.DB_PORT,
+      DB_NAME = var.DB_NAME,
+      DB_USERNAME = var.DB_USERNAME,
+      DB_PASSWORD = var.DB_PASSWORD
+    }
+  }
+
+  memory_size = 512
+  timeout     = 30
+}
+
 # DASHBOARD ECS TASK DEFINITION
 # resource "aws_ecs_task_definition" "c19-cran-plants-dashboard" {
 #   family = "c19-cran-plants-dashboard"
@@ -150,41 +195,4 @@ resource "aws_ecs_task_definition" "c19-cran-plants-pipeline" {
 #     }
 #   ]
 #   )
-# }
-
-# Lambda Function
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "c19-cran-lambda-role" {
-  name               = "c19-cran-lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-# resource "aws_lambda_function" "c19-cran-plants-summarise-lambda" {
-#   function_name = "c19-cran-plants-summarise"
-#   role          = "arn:aws:iam::129033205317:role/c19-cran-lambda-role"
-#   package_type  = "Image"
-#   image_uri     = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c19-cran-summarise:latest"
-
-#   environment {
-#     variables = {
-#       ACCESS_KEY = var.ACCESS_KEY,
-#       SECRET_ACCESS_KEY = var.SECRET_ACCESS_KEY
-#     }
-#   }
-
-#   memory_size = 512
-#   timeout     = 30
 # }
