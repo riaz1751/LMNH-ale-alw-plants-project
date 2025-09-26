447b316bed86fe11a9998896ebbed88ae11d17b2
Nicanor Enguerra <nenguerra@gmail.com>
2025-09-26 10:03:04 +0100

    Add dashboard terraform ecs role and td

--- terraform/stage_two/main.tf
+++ terraform/stage_two/main.tf
@@ -145,9 +145,16 @@ resource "aws_lambda_function" "c19-cran-plants-summarise-lambda" {
 #   execution_role_arn = aws_iam_role.c19-cran-task-execution-role.arn
 #   container_definitions = jsonencode([
 #     {
-#       name = "my_pipeline",
-#       image ="129033205317.dkr.ecr.eu-west-2.amazonaws.com/c19-cran-plants-dashboard:latest",
+#       name = "c19-cran-plants-dashboard",
+#       image ="",
 #       essential = true,
+#       portMappings = [
+#         {
+#           containerPort = 8501
+#           hostPort      = 8501
+#           protocol      = "tcp"
+#         }
+#       ],
 #       logConfiguration = {
 #                 logDriver = "awslogs"
 #                 "options": {
@@ -190,9 +197,74 @@ resource "aws_lambda_function" "c19-cran-plants-summarise-lambda" {
 #         {
 #           name = "S3_NAME",
 #           value = var.S3_NAME
+#         },
+#         {
+#           name = "DB_DRIVER",
+#           value = var.DB_DRIVER
 #         }
 #       ]
 #     }
 #   ]
 #   )
+#   runtime_platform {
+#     operating_system_family = "LINUX"
+#     cpu_architecture        = "X86_64"
+#   }
+# }
+
+resource "aws_iam_role" "c19_cran_ecs_service_role" {
+  name = "c19-cran_ecs_service_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "c19_cran_ecs_athena" {
+  role       = aws_iam_role.c19_cran_ecs_service_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "c19_cran_ecs_rds" {
+  role       = aws_iam_role.c19_cran_ecs_service_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
+resource "aws_security_group" "c19_cran_sg" {
+  name        = "c19_cran_sg"
+  description = "Allow access to streamlit dashboard"
+  vpc_id      = "vpc-0f29b6a6ab918bcd5"
+
+  ingress {
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# resource "aws_ecs_service" "c19_cran_service" {
+#   name            = "c19_cran_service"
+#   cluster         = "arn:aws:ecs:eu-west-2:129033205317:cluster/c19-ecs-cluster"
+#   task_definition = aws_ecs_task_definition.c19-cran-plants-dashboard
+#   desired_count   = "1"
+#   network_configuration {
+#     subnets          = ["subnet-00506a8db091bdf2a", "subnet-0425a4a0b929ea507", "subnet-0e7a1e60734c4fca7"]
+#     security_groups  = [aws_security_group.c19_cran_sg]
+#     assign_public_ip = true
+#   }
 # }
